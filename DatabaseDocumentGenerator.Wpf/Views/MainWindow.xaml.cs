e8fdb4c1bfeaacf8989e7a8b9953e66f178a9bd9
jinghongbo <jinghongbo@live.com>
2019-07-17 22:27:12 +0800

    update

--- DatabaseDocumentGenerator.Wpf/Views/MainWindow.xaml.cs
+++ DatabaseDocumentGenerator.Wpf/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DatabaseDocumentGenerator.Wpf
+namespace DatabaseDocumentGenerator.Views
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
